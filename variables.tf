f0318f02ea0eed02c039569cc009d9372bf32434
Manideep <fs.eagleop@gmail.com>
2025-04-18 20:34:52 +0530

    added terraform configuration files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0f1dcc636b69a6438" # Update as per your region
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
