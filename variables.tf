371256838638d8933f08789ab8dd692c2586a772
Manideep <fs.eagleop@gmail.com>
2025-04-18 20:41:11 +0530

    updated credentials

--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "ami_id" {
 variable "instance_type" {
   default = "t2.micro"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  default     = ""  # Leave empty, will be passed from environment
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  default     = ""  # Leave empty, will be passed from environment
+}
